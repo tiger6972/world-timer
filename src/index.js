5cab45314edde857fc2521d2afedc6bb3f61ee5b
tiger6972 <140623267+tiger6972@users.noreply.github.com>
2023-08-23 21:50:36 +0100

    Add current timezone to HTML and JS

--- src/index.js
+++ src/index.js
@@ -8,19 +8,22 @@ function updateTime() {
   losAngelesDateElement.innerHTML = moment().format("DD MMMM YYYY");
   losAngelesTimeElement.innerHTML = `${losAngelesTime.format(`HH:mm:ss`)}`;
 
-  //London
+  //Paris
 
-  let londonElement = document.querySelector("#london");
-  let londonDateElement = londonElement.querySelector(".date");
-  let londonTimeElement = londonElement.querySelector(".time");
-  let londonTime = moment().tz(`Europe/London`);
+  let parisElement = document.querySelector("#paris");
+  let parisDateElement = parisElement.querySelector(".date");
+  let parisTimeElement = parisElement.querySelector(".time");
+  let parisTime = moment().tz(`Europe/Paris`);
 
-  londonDateElement.innerHTML = moment().format("DD MMMM YYYY");
-  londonTimeElement.innerHTML = `${londonTime.format(`HH:mm:ss`)}`;
+  parisDateElement.innerHTML = moment().format("DD MMMM YYYY");
+  parisTimeElement.innerHTML = `${parisTime.format(`HH:mm:ss`)}`;
 }
 
 function updateCity(event) {
   let cityTimeZone = event.target.value;
+  if (cityTimeZone === "current") {
+    cityTimeZone = moment.tz.guess();
+  }
   let cityName = cityTimeZone.replace("_", " ").split("/")[1];
   let cityTime = moment().tz(cityTimeZone);
   let citiesElement = document.querySelector("#cities");
