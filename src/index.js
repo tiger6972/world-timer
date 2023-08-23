cd6ada2acf8a2a3a3d72b79a9e5ccb7d18a10527
tiger6972 <140623267+tiger6972@users.noreply.github.com>
2023-08-23 21:08:25 +0100

    update to JS to get local date and local time - 24 hour clock

--- src/index.js
+++ src/index.js
@@ -1,1 +1,23 @@
-alert = "Hello";
+function updateTime() {
+  //Los Angeles
+  let losAngelesElement = document.querySelector("#los-angeles");
+  let losAngelesDateElement = losAngelesElement.querySelector(".date");
+  let losAngelesTimeElement = losAngelesElement.querySelector(".time");
+  let losAngelesTime = moment().tz(`America/Los_Angeles`);
+
+  losAngelesDateElement.innerHTML = moment().format("DD MMMM YYYY");
+  losAngelesTimeElement.innerHTML = `${losAngelesTime.format(`HH:mm:ss`)}`;
+
+  //London
+
+  let londonElement = document.querySelector("#london");
+  let londonDateElement = londonElement.querySelector(".date");
+  let londonTimeElement = londonElement.querySelector(".time");
+  let londonTime = moment().tz(`Europe/London`);
+
+  londonDateElement.innerHTML = moment().format("DD MMMM YYYY");
+  londonTimeElement.innerHTML = `${londonTime.format(`HH:mm:ss`)}`;
+}
+
+updateTime();
+setInterval(updateTime, 1000);
