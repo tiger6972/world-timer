533cd0975522489f5dcb13bb3c3cc84fea7eb3f0
tiger6972 <140623267+tiger6972@users.noreply.github.com>
2023-08-23 21:40:38 +0100

    update to HTML & JS to make dropdown work

--- src/index.js
+++ src/index.js
@@ -19,5 +19,24 @@ function updateTime() {
   londonTimeElement.innerHTML = `${londonTime.format(`HH:mm:ss`)}`;
 }
 
+function updateCity(event) {
+  let cityTimeZone = event.target.value;
+  let cityName = cityTimeZone.replace("_", " ").split("/")[1];
+  let cityTime = moment().tz(cityTimeZone);
+  let citiesElement = document.querySelector("#cities");
+  citiesElement.innerHTML = `
+  <div class="city" id="los-angeles">
+          <div>
+            <h2>${cityTimeZone}</h2>
+            <div class="date">${cityTime.format("DD MMMM YYYY")}</div>
+          </div>
+          <div class="time">${cityTime.format("HH:mm:ss")}"</div>
+        </div>  
+  `;
+}
+
 updateTime();
 setInterval(updateTime, 1000);
+
+let citiesSelectElement = document.querySelector("#city-selection");
+citiesSelectElement.addEventListener("change", updateCity);
